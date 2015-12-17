7af9c90ef182eafc430621c08b96837ac94f4ecc
Jonah <jonah.ahvonen@gmail.com>
2015-12-18 00:53:55 +0200

    Version 1.0

--- lerppu.js
+++ lerppu.js
@@ -3,7 +3,7 @@ var Lerppu =
 	time: 0, // Program / game elapsed time
 	lerps: [],
 
-	interpolate: function(v0, v1, t, f, id)
+	interpolate: function(v0, v1, t, f, easing, id)
 	{
 		var self = this;
 
@@ -16,6 +16,7 @@ var Lerppu =
 			t: t,
 			st: self.time,
 			f: f,
+			easing: easing,
 			id: id
 		});
 	},
@@ -30,7 +31,7 @@ var Lerppu =
 		{
 			var lerp = this.lerps[i];
 			var ct = Math.min((self.time - lerp.st) / lerp.t, 1);
-			var lr = self._easeInOutCubic(lerp.v0, lerp.v1, ct);
+			var lr = lerp.easing(lerp.v0, lerp.v1, ct);
 
 			lerp.f(lr);
 
@@ -60,86 +61,90 @@ var Lerppu =
 		}
 	},
 
-	_lerp: function(v0, v1, t) 
+	easings:
 	{
-		return v0 + t * (v1 - v0);
-	},
 
-	// Accurate
-	_lerp2: function(v0, v1, t) 
-	{
-  		return (1 - t) * v0 + t * v1;
-	},
+		lerp: function(v0, v1, t) 
+		{
+			return v0 + t * (v1 - v0);
+		},
 
-	_easeInQuad: function (v0, v1, t) 
-	{ 
-		return (v1 - v0) * (t*t) + v0; 
-	},
+		// Accurate
+		lerp2: function(v0, v1, t) 
+		{
+	  		return (1 - t) * v0 + t * v1;
+		},
 
-	// decelerating to zero velocity
-	_easeOutQuad: function (v0, v1, t) 
-	{ 
-		return (v1 - v0) * (t*(2-t)) + v0; 
-	},
+		easeInQuad: function (v0, v1, t) 
+		{ 
+			return (v1 - v0) * (t*t) + v0; 
+		},
 
-	// acceleration until halfway, then deceleration
-	_easeInOutQuad: function (v0, v1, t) 
-	{ 
-		return t<.5 ? (v1 - v0) * (2*t*t) + v0 : (v1 - v0) * (-1+(4-2*t)*t) + v0; 
-	},
+		// decelerating to zero velocity
+		easeOutQuad: function (v0, v1, t) 
+		{ 
+			return (v1 - v0) * (t*(2-t)) + v0; 
+		},
 
-	// accelerating from zero velocity 
-	_easeInCubic: function (v0, v1, t) 
-	{ 
-		return (v1 - v0) * (t*t*t) + v0; 
-	},
+		// acceleration until halfway, then deceleration
+		easeInOutQuad: function (v0, v1, t) 
+		{ 
+			return t<.5 ? (v1 - v0) * (2*t*t) + v0 : (v1 - v0) * (-1+(4-2*t)*t) + v0; 
+		},
 
-	// decelerating to zero velocity 
-	_easeOutCubic: function (v0, v1, t) 
-	{ 
-		return (v1 - v0) * ((--t)*t*t+1) + v0;
-	},
+		// accelerating from zero velocity 
+		easeInCubic: function (v0, v1, t) 
+		{ 
+			return (v1 - v0) * (t*t*t) + v0; 
+		},
 
-	// acceleration until halfway, then deceleration 
-	_easeInOutCubic: function (v0, v1, t) 
-	{ 
-		return t<.5 ? (v1 - v0) * (4*t*t*t) + v0 : (v1 - v0) * ((t-1)*(2*t-2)*(2*t-2)+1) + v0; 
-	},
+		// decelerating to zero velocity 
+		easeOutCubic: function (v0, v1, t) 
+		{ 
+			return (v1 - v0) * ((--t)*t*t+1) + v0;
+		},
 
-	// accelerating from zero velocity 
-	_easeInQuart: function (v0, v1, t) 
-	{ 
-		return (v1 - v0) * (t*t*t*t) + v0; 
-	},
+		// acceleration until halfway, then deceleration 
+		easeInOutCubic: function (v0, v1, t) 
+		{ 
+			return t<.5 ? (v1 - v0) * (4*t*t*t) + v0 : (v1 - v0) * ((t-1)*(2*t-2)*(2*t-2)+1) + v0; 
+		},
 
-	// decelerating to zero velocity 
-	_easeOutQuart: function (v0, v1, t) 
-	{
-    	return (v1 - v0) * (1-(--t)*t*t*t) + v0;
-	},
+		// accelerating from zero velocity 
+		easeInQuart: function (v0, v1, t) 
+		{ 
+			return (v1 - v0) * (t*t*t*t) + v0; 
+		},
 
-	// acceleration until halfway, then deceleration
-	_easeInOutQuart: function (v0, v1, t) 
-	{ 
-		return t<.5 ? (v1 - v0) * (8*t*t*t*t) + v0 : (v1 - v0) * (1-8*(--t)*t*t*t) + v0; 
-	},
+		// decelerating to zero velocity 
+		easeOutQuart: function (v0, v1, t) 
+		{
+	    	return (v1 - v0) * (1-(--t)*t*t*t) + v0;
+		},
 
-	// accelerating from zero velocity
-	_easeInQuint: function (v0, v1, t) 
-	{ 
-		return (v1 - v0) * (t*t*t*t*t) + v0; 
-	},
+		// acceleration until halfway, then deceleration
+		easeInOutQuart: function (v0, v1, t) 
+		{ 
+			return t<.5 ? (v1 - v0) * (8*t*t*t*t) + v0 : (v1 - v0) * (1-8*(--t)*t*t*t) + v0; 
+		},
 
-	// decelerating to zero velocity
-	_easeOutQuint: function (v0, v1, t) 
-	{ 
-		return (v1 - v0) * (1+(--t)*t*t*t*t) + v0;
-	},
+		// accelerating from zero velocity
+		easeInQuint: function (v0, v1, t) 
+		{ 
+			return (v1 - v0) * (t*t*t*t*t) + v0; 
+		},
 
-	// acceleration until halfway, then deceleration 
-	_easeInOutQuint: function (v0, v1, t) 
-	{ 
-		return t<.5 ? (v1 - v0) * (16*t*t*t*t*t) + v0 : (v1 - v0) * (1+16*(--t)*t*t*t*t) + v0;
+		// decelerating to zero velocity
+		easeOutQuint: function (v0, v1, t) 
+		{ 
+			return (v1 - v0) * (1+(--t)*t*t*t*t) + v0;
+		},
+
+		// acceleration until halfway, then deceleration 
+		easeInOutQuint: function (v0, v1, t) 
+		{ 
+			return t<.5 ? (v1 - v0) * (16*t*t*t*t*t) + v0 : (v1 - v0) * (1+16*(--t)*t*t*t*t) + v0;
+		}
 	}
 }
 
